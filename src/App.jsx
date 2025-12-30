6e72eef20164d5a65ea3dcd1a1794bfca7f5ef1c
Daniela-Padilha <danielasofiapadilha@gmail.com>
2025-12-30 00:22:48 +0000

    updated app to use header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Header />
+      <main className="container">
+        <Home />
+      </main>
     </div>
   );
 }
