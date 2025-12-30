5fcd3ddc3a32c83f6c1a66f6da1de8aa8f0383c6
Daniela-Padilha <danielasofiapadilha@gmail.com>
2025-12-30 00:23:35 +0000

    added search by keyword

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
+import { useState } from "react";
+import apoios from "../data/apoios.json";
+import ApoioCard from "../components/ApoioCard";
+
 function Home() {
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const filteredApoios = apoios.filter((apoio) => {
+		const text = `${apoio.nome} ${apoio.descricao} ${apoio.categoria}`.toLowerCase();
+		return (text.includes(searchTerm.toLowerCase()));
+	});
+
 	return (
 		<div>
-			<h2>Bem-vindo ao Apoio Fácil</h2>
-			<p>Escolhe uma categoria ou pesquisa um apoio.</p>
+			<h2>Apoios disponíveis</h2>
+			<input 
+				type="text" 
+				placeholder="Pesquisar apoios..."
+				value={searchTerm}
+				onChange={(e) => setSearchTerm(e.target.value)}
+			>
+			</input>
+
+			{filteredApoios.map((apoio) => (
+				<ApoioCard key={apoio.id} apoio={apoio} />
+			))}
+
+			{filteredApoios.length === 0 && (
+				<p>Nenhum apoio encontrado.</p>
+			)}
 		</div>
 	);
 }
